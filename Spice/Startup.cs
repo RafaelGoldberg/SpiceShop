230755dc75fe9a9c58384bcff1e01c0906f1e873
rafig <rafaelgoldberg@outlook.com>
2020-02-27 20:25:22 -0500

    aded VeiwImports and ViewStart to Customer View Folder

--- Spice/Startup.cs
+++ Spice/Startup.cs
@@ -87,8 +87,8 @@ namespace Spice
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+                  name: "areas",
+                  pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
